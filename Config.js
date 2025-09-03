83e1672dbd69160c5582567604ad12a07ba42c63
puncharat893 <pichamon09062537@gmail.com>
2025-09-03 11:28:21 +0700

    Update Config.js
    แก้ไข line liff

--- Config.js
+++ Config.js
@@ -2,6 +2,6 @@
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbycYAnTTkDa8mD2SgmiUvtOUwchGU7MNPYgiZ9YOLWwjBXTFpdSTMETu2TZCjVjj7-3/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbweRUbEmEy7s-7CVI3dgZz7wHgEPY7C-5VnhpNr3hFDWM0G3AzVVdQlaRuE4wiTtnZ0GQ/exec';
 //LIFF_ID
-    const LIFF_ID = '2007475303-d5v41rEq'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2007475303-JYyrOdWn'; // LIFF ID history
-    const LIFF_REGISTER = '2007475303-7wJz410Q'; // LIFF ID register
+    const LIFF_ID = '2008040462-gVP0VXlB'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2008040462-q4XEpg4y'; // LIFF ID history
+    const LIFF_REGISTER = '2008040462-mP3Db7aX'; // LIFF ID register
