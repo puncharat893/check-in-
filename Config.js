c00ece762210564293dc3676270e5637d5eb368b
puncharat893 <pichamon09062537@gmail.com>
2025-09-03 11:04:43 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,7 @@
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbycYAnTTkDa8mD2SgmiUvtOUwchGU7MNPYgiZ9YOLWwjBXTFpdSTMETu2TZCjVjj7-3/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbweRUbEmEy7s-7CVI3dgZz7wHgEPY7C-5VnhpNr3hFDWM0G3AzVVdQlaRuE4wiTtnZ0GQ/exec';
+//LIFF_ID
+    const LIFF_ID = '2007475303-d5v41rEq'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2007475303-JYyrOdWn'; // LIFF ID history
+    const LIFF_REGISTER = '2007475303-7wJz410Q'; // LIFF ID register
