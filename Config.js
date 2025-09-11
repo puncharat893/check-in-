3ea4d6ce180e1a283901f866bc807651e8f2fdd6
puncharat893 <pichamon09062537@gmail.com>
2025-09-11 14:16:41 +0700

    Update Config.js
    แก้ไข

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbz7g79XUhXeMiWe-w1YgQ17Y48J_s7KxImjxBm1O3dcuS_g_QnbvgZQMIYEGzw2YKz3JQ/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz2ooltQA7HBSG49TFMciqPYRhm7Asql8JPq52I4elpkC-iZJAlS_FyCz7HnSh4HBWBEA/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxy2yz7rvNpNmLRb3ztChCS5lIKVOR1yt2upAO4yFBH06gQuFhjmtHIQkC7Km5dWBsVfw/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbynEz10zbqfqXVfQLniF-gkZ2ZALgp4X7aHYIj9MG9QRM2oMOjqP6L8jcpNBT0adcQgMQ/exec';
 //LIFF_ID
     const LIFF_ID = '2008040462-gVP0VXlB'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008040462-q4XEpg4y'; // LIFF ID history
